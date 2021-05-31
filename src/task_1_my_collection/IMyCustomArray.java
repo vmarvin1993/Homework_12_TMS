27098df427700113a56add9528d09a849f992fb8
Виталий Сливец <123@unet.by>
2021-05-31 13:09:48 +0300

    add

--- src/task_1_my_collection/IMyCustomArray.java
+++ src/task_1_my_collection/IMyCustomArray.java
@@ -3,12 +3,19 @@ package task_1_my_collection;
 public interface IMyCustomArray<E> {
 
     public boolean add(Object elem);
-    public E get (int index);
+
+    public E get(int index);
+
     public boolean remove(int index);
+
     public boolean contains(Object elem);
+
     public boolean removeCollection();
+
     public int size();
+
     public void resize(int newLength);
+
     public void add(int index, E elem);
 
 
